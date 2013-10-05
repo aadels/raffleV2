b759afae0192ba8a208377b74d9e23b967187b66
ann adelsberger <adelsbergerann@gmail.com>
2013-10-05 13:47:34 -0400

    added placeholders for all input fields

--- demo.php
+++ demo.php
@@ -15,11 +15,11 @@
 		<!-- form for participant input-->
 		<form method='POST' action ='demo.php'>
 			Enter 5 contestants <br>
-			<input type='text' name='contestant1' placeholder="Contestant One" <?php if($_POST) echo "value=\"{$name[0]}\""?>>
-			<input type='text' name='contestant2'><br>
-			<input type='text' name='contestant3'><br>
-			<input type='text' name='contestant4'><br>
-			<input type='text' name='contestant5'><br>
+			<input type='text' name='contestant1' placeholder="Contestant One" <?php if($_POST) echo "value=\"{$name[0]}\""?>><br>
+			<input type='text' name='contestant2' placeholder="Contestant Two" <?php if($_POST) echo "value=\"{$name[0]}\""?>>><br>
+			<input type='text' name='contestant3' placeholder="Contestant Three" <?php if($_POST) echo "value=\"{$name[0]}\""?>>><br>
+			<input type='text' name='contestant4' placeholder="Contestant Four" <?php if($_POST) echo "value=\"{$name[0]}\""?>>><br>
+			<input type='text' name='contestant5' placeholder="Contestant Five" <?php if($_POST) echo "value=\"{$name[0]}\""?>>><br>
 			<input type='submit'value='Pick a winner!'><br>	
 		</form>
 
