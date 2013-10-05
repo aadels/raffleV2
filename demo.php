ac510f2d08609360cae94ac471a9879f01c2cb09
ann adelsberger <adelsbergerann@gmail.com>
2013-10-05 13:58:04 -0400

    changed placeholders

--- demo.php
+++ demo.php
@@ -15,11 +15,11 @@
 		<!-- form for participant input-->
 		<form method='POST' action ='demo.php'>
 			Enter 5 contestants <br>
-			<input type='text' name='contestant1' placeholder="Contestant name" <?php if($_POST) echo "value=\"{$name[0]}\""?>><br>
-			<input type='text' name='contestant2' placeholder="Contestant name" <?php if($_POST) echo "value=\"{$name[1]}\""?>><br>
-			<input type='text' name='contestant3' placeholder="Contestant name" <?php if($_POST) echo "value=\"{$name[2]}\""?>><br>
-			<input type='text' name='contestant4' placeholder="Contestant name" <?php if($_POST) echo "value=\"{$name[3]}\""?>><br>
-			<input type='text' name='contestant5' placeholder="Contestant name" <?php if($_POST) echo "value=\"{$name[4]}\""?>><br>
+			<input type='text' name='contestant1' placeholder="Contestant 1" <?php if($_POST) echo "value=\"{$name[0]}\""?>><br>
+			<input type='text' name='contestant2' placeholder="Contestant 2" <?php if($_POST) echo "value=\"{$name[1]}\""?>><br>
+			<input type='text' name='contestant3' placeholder="Contestant 3" <?php if($_POST) echo "value=\"{$name[2]}\""?>><br>
+			<input type='text' name='contestant4' placeholder="Contestant 4" <?php if($_POST) echo "value=\"{$name[3]}\""?>><br>
+			<input type='text' name='contestant5' placeholder="Contestant 5" <?php if($_POST) echo "value=\"{$name[4]}\""?>><br>
 			<input type='submit'value='Pick a winner!'><br>	
 		</form>
 
