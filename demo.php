ca715685869611d2a734e4055016f3975a138416
ann adelsberger <adelsbergerann@gmail.com>
2013-10-05 13:35:15 -0400

    added placeholder to one inpur field in demo.php and added array for input names in logic.php

--- demo.php
+++ demo.php
@@ -15,7 +15,7 @@
 		<!-- form for participant input-->
 		<form method='POST' action ='demo.php'>
 			Enter 5 contestants <br>
-			<input type='text' name='contestant1'><br>
+			<input type='text' name='contestant1' placeholder="Contestant One" <?php if($_POST) echo "value=\"{$name[0]}\""><br>
 			<input type='text' name='contestant2'><br>
 			<input type='text' name='contestant3'><br>
 			<input type='text' name='contestant4'><br>
--- logic.php
+++ logic.php
@@ -7,13 +7,21 @@
 		$winningNumber = rand(1,$numberOfContestants);
 
 		/*loop through all contestants*/
+		# $inputName will be the name of the input field such as "contestant1" or "contestant2"
+		# $value will be whatever was typed into that field
+		$count = 0
 		foreach($_POST as $inputName => $value){
-	
-			/*get random number*/
+
+			//Save names in an array
+			$name[$count] = $value;
+			$count ++;
+
+			//get random number
 			$randomNumber = rand(1,$numberOfContestants);
 	
-
-			/*compare random number for each contestant to the wimnning number*/
+			/*Compare random number for each contestant to the wimnning number to 
+			see if there is a wilnner.
+			first test to see if field was filled in.*/
 			if ($value != ""){
 
 				if ($randomNumber == $winningNumber){
